4b0744bb28410c60e0d1eda9ba835a57386fe974
master_6908 <evgenijj.kum@gmail.com>
2019-05-23 18:51:20 +0300

    My Django First app, first commit

---
+++ ll_env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/master/py3/app3/ll_env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ myapp_blog/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MyappBlogConfig(AppConfig):
+    name = 'myapp_blog'
